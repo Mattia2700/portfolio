21acf406320b3d26e77c82d820679240579aae89
Mattia2700 <mattia2700franzin+dev@gmail.com>
2023-04-18 15:40:40 +0200

    feat: add cv

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { saveAs } from 'file-saver';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  mounted() {
+    fetch("./assets/CV - Mattia Franzin.pdf")
+      .then((response) => response.blob())
+      .then((blob) => {
+        saveAs(blob, "CV - Mattia Franzin.pdf");
+      });
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+/* your component styles */
 </style>
